8d9466e15700b731efb8a1ab5b7ba953c3a885bc
Jonnyh Lima <jonhsakamaki1999@gmail.com>
2025-08-25 16:36:37 -0300

    feat: :sparkles: calculando a area de um quadrado, e mostrando o dobro da area

---
+++ Lista/Ex07.py
@@ -0,0 +1,5 @@
+l = float(input("Digite o valor do lado do quadrado: "))
+ar = l * 2 
+ard = ar * 2
+
+print("A área do quadrado é de {:.2f}, e o dobro desta mesma área é de {:.2f}".format(ar, ard))
