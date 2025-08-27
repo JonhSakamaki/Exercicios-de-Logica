3525f76347f6202706b6c2b113c9297bafcf243d
Jonnyh Lima <jonhsakamaki1999@gmail.com>
2025-08-27 18:58:30 -0300

    feat: :sparkles: Convertendo Graus Farenheit para Celsius

---
+++ Lista/Ex09.py
@@ -0,0 +1,5 @@
+f = float(input("Digite quantos graus Farenheit voce quer converter para Celsius: "))
+
+c = (f-32)* 5 / 9
+
+print("Ao converter {:.2f} Graus Farenheit, voce vai ter {:.2f} Graus Celsius".format(f, c))
