881aa4b702cd4aabc504dc0cb52d7d5c3809143d
Jonnyh Lima <jonhsakamaki1999@gmail.com>
2025-09-12 17:34:25 -0300

    feat: :sparkles: Verificando maior numero digitado

---
+++ Lista/Ex19.py
@@ -0,0 +1,7 @@
+n1 = int(input("Digite o Primeiro numero: "))
+n2 = int(input("Digite o Segundo numero: "))
+
+if n1 > n2:
+    print("O maior Numero digitado foi: {}".format(n1))
+else:
+    print("O maior Numero digitado foi: {}".format(n2))
