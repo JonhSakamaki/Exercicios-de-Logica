028172e99bd093483c34ddf5ae7a093fb4721e09
Jonnyh Lima <jonhsakamaki1999@gmail.com>
2025-09-26 00:01:55 -0300

    feat: :sparkles: Positivo ou Negativo

---
+++ Lista/Ex20.py
@@ -0,0 +1,8 @@
+num = int(input("DIgite um numero: "))
+
+if num > 0 :
+    print("O Numero {} é positivo! ".format(num))
+elif num < 0 :
+    print("O Numero {} é negativo! ".format(num))
+else:
+    print("O Numero digitado é {} ".format(num))
